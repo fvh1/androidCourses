177137bbb5c90cac41ff92fc8a32a898f2a32e11
fvh1 <fvh1@tut.by>
2015-09-16 19:51:49 +0300

    Lesson2.1

---
+++ lesson2/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		String arr []= new String [6];
+		for (int i=0; i<arr.length; i++){
+			arr[i]=sc.nextLine();
+		}
+		sc.close();
+		for (String a:arr){
+			System.out.print(a+" ");
+		}
+	}
+
+}
